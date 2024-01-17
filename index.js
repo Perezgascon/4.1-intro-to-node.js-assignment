cc3d72a710abcb590e5a88d08c28eb00f9455172
Perezgascon <76476520+Perezgascon@users.noreply.github.com>
2024-01-17 11:06:57 +0700

    fixed some issues with server

--- index.js
+++ index.js
@@ -11,27 +11,29 @@ const server = http.createServer((req, res) => {
     const pathname = parsedUrl.pathname;
     const query = parsedUrl.query;
 
-    if (pathname === '/profile' || pathname === '/products' || pathname === '/cart' || pathname === '/register' || pathname === '/login') {
+    if (pathname === '/profile' || pathname === '/cart' || pathname === '/register' || pathname === '/login') {
         res.write(`This is the ${pathname} page!`);
+    } else if (pathname === '/products') {
+        res.write(`This is the ${pathname} page!`);
+        
+        if (query && query.search) {
+            const searchKeywords = ['Milk', 'Eggs', 'Cheese', 'Pork', 'Shrimp', 'Chicken'];
+            const searchTerm = query.search.toLowerCase(); 
+        
+            const foundKeyword = searchKeywords.find(keyword => keyword.toLowerCase() === searchTerm);
+        
+            if (foundKeyword) {
+                res.write(` Product "${foundKeyword}" found.`);
+            } else {
+                res.write(` Product "${searchTerm}" not found.`);
+            }
+        } else {
+            res.write(' Search parameter is missing.');
+        }
     } else if (pathname === '/') {
         res.write("Welcome to my site");
     } else {
         res.write(pathname);
-    }
-
-    if (query && query.search) {
-        const searchKeywords = ['Milk', 'Eggs', 'Cheese', 'Pork', 'Shrimp', 'Chicken'];
-        const searchTerm = query.search.toLowerCase(); // Convert to lowercase for case-insensitive comparison
-    
-        const foundKeyword = searchKeywords.find(keyword => keyword.toLowerCase() === searchTerm);
-    
-        if (foundKeyword) {
-            res.write(`Product "${foundKeyword}" found.`);
-        } else {
-            res.write(`Product "${searchTerm}" not found.`);
-        }
-    } else {
-        res.write('Search parameter is missing.');
     }
 
     res.end();
